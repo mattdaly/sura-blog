c3ca9d903e4c0e1ab3da4f97bc890543e1187743
Matt Daly <matt@matthew-daly.com>
2012-02-09 03:27:23 +0000

    Altered background styling

--- src/Sura/Content/Javascripts/Sura/sura-admin.js
+++ src/Sura/Content/Javascripts/Sura/sura-admin.js
@@ -1,6 +1,3 @@
-/*! http://mths.be/placeholder v1.8.7 by @mathias */
-(function(o,m,r){var t="placeholder" in m.createElement("input"),q="placeholder" in m.createElement("textarea"),l=r.fn,k;if(t&&q){k=l.placeholder=function(){return this};k.input=k.textarea=true}else{k=l.placeholder=function(){return this.filter((t?"textarea":":input")+"[placeholder]").not(".placeholder").bind("focus.placeholder",s).bind("blur.placeholder",p).trigger("blur.placeholder").end()};k.input=t;k.textarea=q;r(function(){r(m).delegate("form","submit.placeholder",function(){var a=r(".placeholder",this).each(s);setTimeout(function(){a.each(p)},10)})});r(o).bind("unload.placeholder",function(){r(".placeholder").val("")})}function n(b){var c={},a=/^jQuery\d+$/;r.each(b.attributes,function(d,e){if(e.specified&&!a.test(e.name)){c[e.name]=e.value}});return c}function s(){var a=r(this);if(a.val()===a.attr("placeholder")&&a.hasClass("placeholder")){if(a.data("placeholder-password")){a.hide().next().show().focus().attr("id",a.removeAttr("id").data("placeholder-id"))}else{a.val("").removeClass("placeholder")}}}function p(){var d,e=r(this),c=e,a=this.id;if(e.val()===""){if(e.is(":password")){if(!e.data("placeholder-textinput")){try{d=e.clone().attr({type:"text"})}catch(b){d=r("<input>").attr(r.extend(n(this),{type:"text"}))}d.removeAttr("name").data("placeholder-password",true).data("placeholder-id",a).bind("focus.placeholder",s);e.data("placeholder-textinput",d).data("placeholder-id",a).before(d)}e=e.removeAttr("id").hide().prev().attr("id",a).show()}e.addClass("placeholder").val(e.attr("placeholder"))}else{e.removeClass("placeholder")}}}(this,document,jQuery));
-
 $(document).ready(function () {
     var page = window.location.pathname.split('admin/')[1];
     var sub = page.indexOf('/');
@@ -8,8 +5,7 @@ $(document).ready(function () {
         page = page.substr(0, sub) + '-' + page.substr(sub + 1);
     }
 
-    $('li.nav-' + page).addClass('current-page');
-    $('input, textarea').placeholder();
+    $('li.nav-' + page).addClass('active');
 });
 
 function redirect(url) {
